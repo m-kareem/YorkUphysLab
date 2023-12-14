adae1a85c983dc08afd38cbab939fdc27cd73781
Mohammad Kareem <mkareem@yorku.ca>
2023-12-14 14:55:28 -0500

    TBS1000 fully implemented

--- YorkUphysLab/Tektronix/TBS1000.py
+++ YorkUphysLab/Tektronix/TBS1000.py
@@ -1,6 +1,6 @@
 import visa # http://github.com/hgrecco/pyvisa
 import numpy as np
-
+import time
 class TBS1000:
     """
     A class representing representing the Tektronix TBS1000 oscilloscope.
@@ -35,7 +35,7 @@ class TBS1000:
         self.inst = None
         self.is_open = False
 
-    
+    #----------------------------------------------
     def connect(self):
         rm = visa.ResourceManager()
         resources_list = rm.list_resources()
@@ -60,7 +60,7 @@ class TBS1000:
         else:
             print('No Tektronix TBS scope was found!')
             return False
-    
+    #----------------------------------------------
     def close(self):
         if self.is_connected():
             self.inst.close()
@@ -69,9 +69,11 @@ class TBS1000:
         else:
             print('Tektronix TBS scope is not connected!')
 
+    #----------------------------------------------
     def is_connected(self):
         return self.is_open
 
+    #----------------------------------------------
     def get_idn(self):
         if self.is_connected():
             return self.inst.query('*idn?')
@@ -79,7 +81,18 @@ class TBS1000:
             return None
     
 
-    def get_data(self, channel=1):
+    #----------------------------------------------
+    def get_period(self, channel):
+        if channel not in [1,2]:
+            raise ValueError("Channel must be 1 or 2")
+        self.inst.write('MEASUrement:IMMed:TYPE PERiod')
+        self.inst.write(f'MEASUrement:IMMed:SOUrce CH{channel}')
+        measured_period = self.inst.query('MEASUrement:IMMed:VALue?')
+        
+        return float(measured_period) # in seconds
+    
+    #----------------------------------------------
+    def get_data(self, channel):
         """
         Retrieves the waveform data from the Tektronix TBS1000 oscilloscope.
 
@@ -106,57 +119,73 @@ class TBS1000:
         """
         if not self.is_connected():
             return None
-        if channel not in ['multi', 'diff', 1, 2]:
-            raise ValueError("Channel must be 'multi', 'diff', 1, or 2")
+        if channel not in [1, 2]:
+            raise ValueError("Channel must be 1, or 2")
 
         scaled_wave = []
 
-        channels = [1, 2] if channel in ['multi', 'diff'] else [channel]
-        for ch in channels:
-            self.inst.write('*rst')  # reset the instrument to a known state.
-            r = self.inst.query('*opc?')  # queries the instrument to check if it has completed the previous operation.
-            self.inst.write('autoset EXECUTE')  # autoset: automatically adjusts the oscilloscope's settings based on the input signal
-            r = self.inst.query('*opc?')
-            # io config
-            self.inst.write('header 0')
-            self.inst.write('data:encdg RIBINARY')
-            self.inst.write(f'data:source CH{ch}')  # channel
-            self.inst.write('data:start 1')  # first sample
-            record = int(self.inst.query('wfmpre:nr_pt?'))  # number of samples
-            self.inst.write(f'data:stop {record}')  # last sample
-            self.inst.write('wfmpre:byt_nr 1')  # 1 byte per sample
-            # acq config
-            self.inst.write('acquire:state 0')  # stop data acquisition
-            self.inst.write('acquire:stopafter SEQUENCE')  # sets the acquisition mode to 'SEQUENCE': acquires a single waveform and then stops
-            self.inst.write('acquire:state 1')  # run
-            r = self.inst.query('*opc?')  # sync
-            bin_wave = self.inst.query_binary_values('curve?', datatype='b', container=np.array)
-            tscale = float(self.inst.query('wfmpre:xincr?'))  # retrieve scaling factors
-            tstart = float(self.inst.query('wfmpre:xzero?'))
-            vscale = float(self.inst.query('wfmpre:ymult?'))  # volts / level
-            voff = float(self.inst.query('wfmpre:yzero?'))  # reference voltage
-            vpos = float(self.inst.query('wfmpre:yoff?'))  # reference position (level)
-
-            r = int(self.inst.query('*esr?'))  # error checking
-            if r != 0b00000000:
-                print('event status register: 0b{:08b}'.format(r))
-            r = self.inst.query('allev?').strip()
-            if 'No events' not in r:
-                print('all event messages: {}'.format(r))
-            
-            total_time = tscale * record  # create scaled vectors
-            tstop = tstart + total_time
-            scaled_time = np.linspace(tstart, tstop, num=record, endpoint=False) * 1000  # time in ms
-            unscaled_wave = np.array(bin_wave, dtype='double')  # data type conversion
-            _scaled_wave = (unscaled_wave - vpos) * vscale + voff
-            scaled_wave.append(_scaled_wave)
-
-        if channel == 'diff':
-            tmp = scaled_wave[1] - scaled_wave[0]
-            scaled_wave = [tmp]
-
-        return scaled_time, scaled_wave
+        self.inst.write('*rst')  # reset the instrument to a known state.
+        r = self.inst.query('*opc?')  # queries the instrument to check if it has completed the previous operation.
+        self.inst.write('autoset EXECUTE')  # autoset: automatically adjusts the oscilloscope's settings based on the input signal
+        r = self.inst.query('*opc?')
+        # io config
+        self.inst.write('header 0')
+        self.inst.write('data:encdg RIBINARY')
+        self.inst.write(f'data:source CH{channel}')  # channel
+        self.inst.write('data:start 1')  # first sample
+        record = int(self.inst.query('wfmpre:nr_pt?'))  # number of samples
+        self.inst.write(f'data:stop {record}')  # last sample
+        self.inst.write('wfmpre:byt_nr 1')  # 1 byte per sample
+        # acq config
+        self.inst.write('acquire:state 0')  # stop data acquisition
+        self.inst.write('acquire:stopafter SEQUENCE')  # sets the acquisition mode to 'SEQUENCE': acquires a single waveform and then stops
+        self.inst.write('acquire:state 1')  # run
+        r = self.inst.query('*opc?')  # sync
+        bin_wave = self.inst.query_binary_values('curve?', datatype='b', container=np.array)
+        tscale = float(self.inst.query('wfmpre:xincr?'))  # retrieve scaling factors
+        tstart = float(self.inst.query('wfmpre:xzero?'))
+        vscale = float(self.inst.query('wfmpre:ymult?'))  # volts / level
+        voff = float(self.inst.query('wfmpre:yzero?'))  # reference voltage
+        vpos = float(self.inst.query('wfmpre:yoff?'))  # reference position (level)
+        r = int(self.inst.query('*esr?'))  # error checking
+        if r != 0b00000000:
+            print('event status register: 0b{:08b}'.format(r))
+        r = self.inst.query('allev?').strip()
+        if 'No events' not in r:
+            print('all event messages: {}'.format(r))
         
+        total_time = tscale * record  # create scaled vectors
+        tstop = tstart + total_time
+        scaled_time = np.linspace(tstart, tstop, num=record, endpoint=False) * 1000  # time in ms
+        
+        unscaled_wave = np.array(bin_wave, dtype='double')  # data type conversion
+        scaled_wave = (unscaled_wave - vpos) * vscale + voff
+                   
+        return scaled_time, scaled_wave, total_time
+    
+    
+    #----------------------------------------------
+    def phase_shift(self, scaled_time, waveform_1, waveform_2, total_time, phase_shift):
+        _, phase = divmod(phase_shift, 360)
+        # get period of waveform 2
+        period = self.get_period(2)
+
+        samples_in_period = int(period/total_time * len(waveform_2)) # number of samples in one period (samples
+
+        shift_samples = int((phase / 360) * samples_in_period) # number of samples to shift
+        
+        print(f"phase shift: {phase} deg, shift_samples: {shift_samples}, total samples: {len(waveform_2)}")
+        
+        shifted_waveform_2 = np.roll(waveform_2, shift_samples)
+
+        return scaled_time[shift_samples:], waveform_1[shift_samples:], shifted_waveform_2[shift_samples:]
+
+    def mix_waveforms(self, waveform_1, waveform_2):
+        if len(waveform_1) != len(waveform_2):
+            error = f"Waveforms must be the same length: {len(waveform_1)} != {len(waveform_2)}"
+            return error
+        
+        return [x*y for x,y in zip(waveform_1,waveform_2)]
 #----------------------------------------------
 # how to use this class
 if __name__ == '__main__':
@@ -164,43 +193,50 @@ if __name__ == '__main__':
     scope = TBS1000()
     # connect to the scope
     scope.connect()
-    #ch = 'multi'
-    ch = 'diff'
-    #ch = 1
+    
+    color = {1:'orange', 2:'blue', 'mix':'red'}
+
     try:
-        scaled_time, scaled_wave = scope.get_data(channel=ch)
-        colors = {1:'orange', 2:'blue', 'diff':'green'}
+        # retrieve waveform data
+        scaled_time, scaled_wave_1, total_time = scope.get_data(channel=1)
+        print('ch 1 complete')
+        time.sleep(12)
+        scaled_time, scaled_wave_2, total_time = scope.get_data(channel=2)
+        print('ch 2 complete')
+        
+        phase_shift = 30
+        stime, wf1,wf2 = scope.phase_shift(scaled_time, scaled_wave_1, scaled_wave_2, total_time, phase_shift)
+
+        mix_wf = scope.mix_waveforms(wf1, wf2)
+        avg_mix_wf = np.average(mix_wf)
         
         # --plotting
-        '''
+        #'''
         import pylab as pl
-        if ch in [1,2]:
-            pl.plot(scaled_time, scaled_wave[0], label=f'Ch {ch}', color=colors[ch])
-            y_max = max(scaled_wave[0])  # find the maximum y value
-        
-        elif ch == 'multi':
-            pl.plot(scaled_time, scaled_wave[0], label=f'Ch 1', color=colors[1])
-            pl.plot(scaled_time, scaled_wave[1], label=f'Ch 2', color=colors[2])
-            y_max = max(max(scaled_wave[0]), max(scaled_wave[1]))
-        
-        elif ch == 'diff':
-            pl.plot(scaled_time, scaled_wave[0], label=f'Ch 2 - Ch 1', color=colors['diff'])
-            y_max = max(scaled_wave[0])
-        
-        pl.ylim(top=y_max*1.3)
+        pl.plot(stime, wf1, label=f'Ch 1, 100Hz', color=color[1])
+        pl.plot(stime, wf2, label=f'Ch 2, 70 Hz', color=color[2])
+        pl.plot(stime, mix_wf, label=f'Mix', color=color['mix'])
+        y_max = max(max(wf1), max(wf2), max(mix_wf))
+       
+        pl.ylim(top=y_max*1.5)
         pl.xlabel('time [ms]') # x label
         pl.ylabel('voltage [v]') # y label
         # Add legend
         pl.legend(loc='upper right')
-        '''
+        
+        pl.rc('grid', linestyle=':', color='gray', linewidth=1)
+        pl.grid(True)
+        pl.title(f'avg_mix_wf: {round(avg_mix_wf,2)}, d_phi: {phase_shift}', fontsize = 14)
+        #'''
         
     except ValueError as e:
         print(e)
     
+
     scope.close()
     
-    """
+    #"""
     print("\nlook for plot window...")
     pl.show()
     print("\nend of demonstration")
-    """
+    #"""
